6cbdeff77d0a732e577afeaf419dc4d1863d3ea7
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-24 13:48:02 +0200

    Added text and image to webpage

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Hello vue',
+            imageSrc: 'img/user-profile-avatar.jpg'
+        }
+    }
+}).mount(' #app ');
